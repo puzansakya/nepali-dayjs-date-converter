b2c05c52a770393c7b0bc091c101522a8cdd7829
pujan shakya <pujan@rigonepal.com>
2024-06-30 23:12:29 +0545

    Fresh build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,24 +1,24 @@
-declare module 'nepali-date-carburettor-xx/ad-to-bs/index' {
+declare module 'nepali-dayjs-date-converter/ad-to-bs/index' {
   export const ADToBS: (date: string) => string | undefined;
 
 }
-declare module 'nepali-date-carburettor-xx/ad-to-bs/index.spec' {
+declare module 'nepali-dayjs-date-converter/ad-to-bs/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/bs-to-ad/index' {
+declare module 'nepali-dayjs-date-converter/bs-to-ad/index' {
   export const BSToAD: (date: string) => string | undefined;
 
 }
-declare module 'nepali-date-carburettor-xx/bs-to-ad/index.spec' {
+declare module 'nepali-dayjs-date-converter/bs-to-ad/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/get-days-in-month-map/index' {
+declare module 'nepali-dayjs-date-converter/get-days-in-month-map/index' {
   export const getDaysInMonthMap: () => Map<any, any>;
 
 }
-declare module 'nepali-date-carburettor-xx/get-english-date/index' {
+declare module 'nepali-dayjs-date-converter/get-english-date/index' {
   export const getEnglishDate: (nepYY: number, nepMM: number, nepDD: number) => {
       year: number;
       month: number;
@@ -26,11 +26,11 @@ declare module 'nepali-date-carburettor-xx/get-english-date/index' {
   };
 
 }
-declare module 'nepali-date-carburettor-xx/get-english-date/index.spec' {
+declare module 'nepali-dayjs-date-converter/get-english-date/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/get-nepali-date/index' {
+declare module 'nepali-dayjs-date-converter/get-nepali-date/index' {
   export const getNepaliDate: (engYY: number, engMM: number, engDD: number) => {
       year: number;
       month: number;
@@ -38,57 +38,57 @@ declare module 'nepali-date-carburettor-xx/get-nepali-date/index' {
   };
 
 }
-declare module 'nepali-date-carburettor-xx/get-nepali-date/index.spec' {
+declare module 'nepali-dayjs-date-converter/get-nepali-date/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/index' {
-  export { ADToBS } from 'nepali-date-carburettor-xx/ad-to-bs/index';
-  export { BSToAD } from 'nepali-date-carburettor-xx/bs-to-ad/index';
-  export { getDaysInMonthMap as lookUp } from 'nepali-date-carburettor-xx/get-days-in-month-map/index';
+declare module 'nepali-dayjs-date-converter/index' {
+  export { ADToBS } from 'nepali-dayjs-date-converter/ad-to-bs/index';
+  export { BSToAD } from 'nepali-dayjs-date-converter/bs-to-ad/index';
+  export { getDaysInMonthMap as lookUp } from 'nepali-dayjs-date-converter/get-days-in-month-map/index';
 
 }
-declare module 'nepali-date-carburettor-xx/is-eng-date-in-conversion-range/index' {
+declare module 'nepali-dayjs-date-converter/is-eng-date-in-conversion-range/index' {
   export const isEngDateInConversionRange: (yy: number, mm: number, dd: number) => boolean;
 
 }
-declare module 'nepali-date-carburettor-xx/is-eng-date-in-conversion-range/index.spec' {
+declare module 'nepali-dayjs-date-converter/is-eng-date-in-conversion-range/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/is-eng-leap-year/index' {
+declare module 'nepali-dayjs-date-converter/is-eng-leap-year/index' {
   export const isEngLeapYear: (year: number) => boolean;
 
 }
-declare module 'nepali-date-carburettor-xx/is-eng-leap-year/index.spec' {
+declare module 'nepali-dayjs-date-converter/is-eng-leap-year/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/is-nep-date-in-conversion-range/index' {
+declare module 'nepali-dayjs-date-converter/is-nep-date-in-conversion-range/index' {
   export const isNepDateInConversionRange: (yy: number, mm: number, dd: number) => boolean;
 
 }
-declare module 'nepali-date-carburettor-xx/is-nep-date-in-conversion-range/index.spec' {
+declare module 'nepali-dayjs-date-converter/is-nep-date-in-conversion-range/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/stitch-date/index' {
+declare module 'nepali-dayjs-date-converter/stitch-date/index' {
   export const stitchDate: (yy: number, mm: number, dd: number) => string;
 
 }
-declare module 'nepali-date-carburettor-xx/stitch-date/index.spec' {
+declare module 'nepali-dayjs-date-converter/stitch-date/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx/zero-pad/index' {
+declare module 'nepali-dayjs-date-converter/zero-pad/index' {
   export const zeroPad: (x: number) => string;
 
 }
-declare module 'nepali-date-carburettor-xx/zero-pad/index.spec' {
+declare module 'nepali-dayjs-date-converter/zero-pad/index.spec' {
   export {};
 
 }
-declare module 'nepali-date-carburettor-xx' {
-  import main = require('nepali-date-carburettor-xx/src/index');
+declare module 'nepali-dayjs-date-converter' {
+  import main = require('nepali-dayjs-date-converter/src/index');
   export = main;
 }
